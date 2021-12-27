755ba42795e9f338e8baaa9a5505422f99380442
Leesin0222 <dragon02229@naver.com>
2021-12-27 12:10:19 +0900

    📑 :: viewBinding 추가

--- moneyconverter/app/src/main/java/com/yongjincompany/moneyconverter/MainActivity.kt
+++ moneyconverter/app/src/main/java/com/yongjincompany/moneyconverter/MainActivity.kt
@@ -2,10 +2,17 @@ package com.yongjincompany.moneyconverter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yongjincompany.moneyconverter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
